db04644716afa995ff40cd0a9d14cf4fed201281
league <league@192.168.1.200>
2024-03-25 18:30:33 -0700

    League Invaders(Game Panel)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 public class LeagueInvaders {
 
 	JFrame frame;
+	GamePanel gp;
 	public static final int HEIGHT = 800;
 	public static final int WIDTH = 500;
 	
@@ -14,12 +15,13 @@ public class LeagueInvaders {
 	
 	LeagueInvaders(){
 		frame = new JFrame();
-		
+		gp = new GamePanel();
 	
 		
 	}
 	
 	void setup() {
+		frame.add(gp);
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
