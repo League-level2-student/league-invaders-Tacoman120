93c7ee8a195d263e4466425d98cd6cfc8897796f
league <asherwelsbie@icloud.com>
2024-04-01 16:59:24 -0700

    League Invaders(IP), Game Panel

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -25,6 +25,7 @@ public class LeagueInvaders {
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.addKeyListener(gp);
 	}
 	
 }
